78f16f210a792aee15841bcfb0aec741be7b28ac
cmulk <cjmulk2@gmail.com>
2020-05-18 19:27:02 -0500

    Update to accommodate changes in the API

--- chartmaker.py
+++ chartmaker.py
@@ -5,7 +5,7 @@ from datetime import date,timedelta
 
 USER = os.environ['USER']
 PASS = os.environ['PASS']
-ESSID = os.environ['ESSID']
+ESIID = os.environ['ESIID']
 OUTFILE = os.environ['OUTFILE']
 NUMDAYS = int(os.environ['NUMDAYS'])
 
@@ -19,23 +19,24 @@ tx = MeterReader()
 
 r = tx.login(USER, PASS)
 
-j = tx.get_daily_read(ESSID, startDate, endDate)
+j = tx.get_daily_read(ESIID, startDate, endDate)
+
 
 print("Creating Bar Chart...")
-rd = j['data']['registeredReads']
+rd = j['dailyData']
 
 
 labels = {
     "x" : "Date",
-    "energyDataKwh": "KwH"
+    "date": "KwH"
 }
 
 # strip year from readDates
-read_dates_short = [data['readDate'][:-5] for data in rd]
+read_dates_short = [data['date'][:-5] for data in rd]
 
-fig = px.bar(data_frame=rd, x=read_dates_short, y ="energyDataKwh", text='energyDataKwh', labels=labels)
+fig = px.bar(data_frame=rd, x=read_dates_short, y ="reading", text='reading', labels=labels)
 fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
 
-fig.write_html(OUTFILE)
+fig.write_html(OUTFILE, include_plotlyjs='cdn')
 
 print("Done.")
